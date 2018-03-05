570eb6b623f553871209a9c2067bb54c227d3890
DrDragonKiller <pwesley.12@gmail.com>
2018-03-05 15:23:18 +0100

    updated program.ml
    current features:
    - print funcions for everything
    - create table for tablefilling
    - step 1: check compare final states with different states
    - step 2: recursive look-up

--- Ocaml test/program.ml
+++ Ocaml test/program.ml
@@ -1,5 +1,6 @@
 (* ~~~~~~~~~~~~~~~~~~~~~~~~ Typ-Definition ~~~~~~~~~~~~~~~~~~~~~~~~ *)
 
+
 type state_type = S | F | N | SF;; (* S = Startzustand; F = Finalzustand; N = Normaler Zustand *)
 
 type dfa_transition = state_type * int * int * int;; 
@@ -13,8 +14,62 @@ type equivalence_result = bool * min_dfa_transition_table;;
 	type nfa_transition_table = nfa_transition list;;
 
 
+(* ~~~~~~~~~~~~~~~~~~~~~~~~ Variabeln ~~~~~~~~~~~~~~~~~~~~~~~~ *)
+let tabelle1  = [(S,1,1,2);(N,2,3,4);(F,3,3,2);(F,4,3,2)] ;; (* DEA 1 *)
+let tabelle2  = [(S,5,6,7);(N,6,5,8);(N,7,9,9);(N,8,9,9);(F,9,9,8);(F,10,10,9)] ;; (* DEA 2 *)
+let tabelle1min  = [(S,1,1,2);(N,2,3,3);(F,3,3,2)] ;; (* DEA 1 *)
+let tabelle2min  = [(S,5,6,7);(N,6,5,8);(N,7,9,9);(N,8,9,9);(F,9,9,8)] ;; (* DEA 2 *)
+let candidates  = (tabelle1min, tabelle2min);;
+
+let (tab1, tab2) = candidates;;
+let candidateList = tab1 @ tab2;;
+
+
 
 (* ~~~~~~~~~~~~~~~~~~ Allgemeine Funktionen ~~~~~~~~~~~~~~~~~~~ *)
+let ( ! ) a = not a;;
+let xor x y = (x && !y)||(!x && y);;
+let ( % ) dividend divisor = dividend mod divisor;;
+
+
+let rec getNElement ls element =
+	if element < 0 then failwith "Out of bounds";
+	match ls with
+	| [] -> failwith "Out of bounds"
+	| hd::tl -> if element=0 then hd else getNElement tl (element-1)
+;;
+
+let rec setNElement ls element value=
+	if element < 0 then failwith "Out of bounds";
+	match ls with
+	| [] -> failwith "Out of bounds"
+	| hd::tl -> if element=0 
+		then value::tl 
+		else hd::(setNElement tl (element-1) value)
+;;
+
+let print_boolInt expression  =
+	match expression with
+	| true -> print_int 1  
+	| false -> print_int 0
+;;
+
+let print_bool expression  =
+	match expression with
+	| true -> print_string "true"  
+	| false -> print_string "false"  
+;;
+
+let rec print_boolean_table ls row column =
+	let rec recursion i =
+		if i < (row * column) 
+			then( print_boolInt (getNElement ls i); print_string "|";
+			(if ((i+1) % column = 0)
+				then print_string "\n"
+			); recursion (i+1));
+	in recursion 0; print_string "\n"
+;;
+
 let rec print_int_list ls = 
 	match ls with
 	| [] -> ()
@@ -30,7 +85,6 @@ let rec print_dfa_transition (state_type, name, partner0, partner1) =
 	print_int name; print_string " | ";
 	print_int partner0; print_string " | ";
 	print_int partner1; print_string "\n"
-
 ;;
 
 let rec print_dfa_transition_table dfa_transition_table = 
@@ -49,13 +103,25 @@ let rec lenght ls =
 	| hd::tl -> 1+(lenght tl)
 ;;
 
-let square x = x*x;;
-
 let rec contains element ls = 
 	match ls with
 	| hd::tl -> if (hd = element) then true else (contains element tl)
 	| []   -> false
 ;;
+
+let rec containsAmount element ls =
+	match ls with
+	| hd::tl -> if (hd = element) 
+		then (containsAmount element tl) +1
+		else (containsAmount element tl)
+	| []   -> 0
+;;	
+
+
+
+
+
+
 (* ~~~~~~~~~~~~~~~~~~~~~~~~ Funktionen ~~~~~~~~~~~~~~~~~~~~~~~~ *)
 let rec make ls count element = 
 	match ls with
@@ -63,14 +129,127 @@ let rec make ls count element =
     | hd::tl -> hd :: (make tl count element)
 ;;
 
+let isStart zustand = 
+	match zustand with s -> s = S || s = SF
+;;
 
-(* ~~~~~~~~~~~~~~~~~~~~~~~~ Variabeln ~~~~~~~~~~~~~~~~~~~~~~~~ *)
-let tabelle1  = [(S,1,1,2);(N,2,3,4);(F,3,3,2);(F,4,3,2)] ;; (* DEA 1 *)
-let tabelle2  = [(S,5,6,7);(N,6,5,8);(N,7,9,9);(N,8,9,9);(F,9,9,8);(F,10,10,9)] ;; (* DEA 2 *)
-let tablecan  = (tabelle1, tabelle2);;
+let isFinal zustand = 
+	match zustand with s -> s = F || s = SF
+;;
+
+
+let getTransitionByPoint knot =
+	let rec recursion ls = 
+		match ls with
+		| [] -> failwith "Unknown knot"
+		| hd::tl -> (
+			let (_, k, t0, t1) = hd in
+			if (knot = k) then ((t0, t1)) else (recursion tl)
+		);
+	in recursion candidateList
+;;
+	
+let getPositioninTable knot = 
+	let rec recursion ls i = 
+		match ls with
+		| [] -> failwith ("Unknown knot: " ^ (string_of_int knot))
+		| hd::tl -> (
+			let (_, k, _, _) = hd in
+			if (knot = k) then i else (recursion tl (i+1))
+		);
+	in recursion candidateList 0
+;;
+	
+let rowWidth, columnHeight = (lenght tab1)+(lenght tab2), (lenght tab1)+(lenght tab2);;
+
+let encode2D row column = ((column * columnHeight) + row);;
+
+let decode2D value = (value / columnHeight, value % columnHeight);;
+
+
+
+
+
+
+let forl func condition reversecondition endvalue ls =
+	let rec recursion ls i =
+		if i <= endvalue 
+			then (
+				if (if reversecondition then (not (condition ls i)) else (condition ls i))
+					then recursion (setNElement ls i (func ls i)) (i+1)
+					else recursion ls (i+1)
+				)
+			else ls
+	in recursion ls 0
+;;
+
+let beTrue _ _ = 
+	true
+;;
+
+let streiche ls element =
+	let (x,y) = decode2D element in
+	let (_, a, a0, a1) = (getNElement candidateList y) in
+	let (_, b, b0, b1) = (getNElement candidateList x) in
+		(getNElement ls (encode2D (getPositioninTable a0) (getPositioninTable b0))) 
+	||	(getNElement ls (encode2D (getPositioninTable a1) (getPositioninTable b1)))
+;;
+
+let areDifferentState ls where =
+	let (x, y) = (decode2D where) in
+	let (sx, _, _, _), (sy, _, _, _) = getNElement candidateList x, getNElement candidateList y in
+	if (xor (isFinal sx) (isFinal sy)) then true else false
+;;
+
+
 
 (* ~~~~~~~~~~~~~~~~~~~~~~~~ Main-Programm ~~~~~~~~~~~~~~~~~~~~~~~~ *)
 
-print_candidates tablecan;;
-let (tab1, tab2) = tablecan;;
-let filling_table = make [] (square ((lenght tab1) + (lenght tab2))) false;; (* true -> angekreuzt *)
+print_candidates candidates;;
+print_string "\n";;
+
+let filling_table = make [] (rowWidth * columnHeight) false;; (* true -> angekreuzt *)
+(* let tabelle = (filling_table, rowWidth, columnHeight);; *)
+
+print_boolean_table filling_table rowWidth columnHeight;; 
+
+let filling_table = forl beTrue areDifferentState false ((rowWidth * columnHeight)-1) filling_table;;
+print_boolean_table filling_table rowWidth columnHeight;;
+
+(* let filling_table = forl streiche getNElement true ((rowWidth * columnHeight)-1) filling_table;; *)
+(* print_boolean_table filling_table rowWidth columnHeight;; *)
+
+let filling_table =
+	let rec recursion ft = (
+		(* print_boolean_table ft rowWidth columnHeight; *)
+		let new_filling_table = (forl streiche getNElement true ((rowWidth * columnHeight)-1) ft) in
+		if (ft = new_filling_table)
+			then (ft)
+			else (print_boolean_table new_filling_table rowWidth columnHeight; recursion new_filling_table)
+			(* else (recursion new_filling_table) *)
+	);
+	in recursion filling_table
+;;
+(*
+let allaequivalenzklassen =
+	let rec recursion =
+		
+	in recursion
+;; *)
+(* print_boolean_table filling_table rowWidth columnHeight;; *)
+
+
+let ausgabae =
+	match (containsAmount false filling_table) with
+	| i when i = rowWidth -> (false, [])
+	| i when i > rowWidth -> ( true,
+[]
+
+
+
+
+)
+
+
+	| i -> failwith ("Less falses then expected (" ^ string_of_int i ^ " instead of at least " ^ string_of_int rowWidth ^ ")")
+;;
